b0bc31453f7897f26c67757e8828fc81ca55bcb1
wd487456869 <487456869@qq.com>
2022-04-26 11:49:29 +0800

    HolleWorld

---
+++ src/main/java/com/example/controller.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author：小丹ASUS
+ * @DateTime: 2022-04-26
+ * 说明：
+ */
+public class controller {
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+}
